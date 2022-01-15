3f1f344b628ed3691fdc76360c3027aa796faf12
Cadez650 <firemancade@outlook.com>
2022-01-15 15:51:56 -0800

    d
    d

--- main.lua
+++ main.lua
@@ -2,4 +2,4 @@ local function getScript(URL)
     return game:HttpGet('https://raw.githubusercontent.com/Cadez650/VapeCustom/main/'..url, true)
 end
 
-local mainVape = getScript('main.vape')
+local mainVape = loadstring(getScript('best.lua'))
