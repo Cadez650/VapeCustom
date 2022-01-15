afa40959229e6f430f956b1188f9d3a99987c3fb
Cadez650 <firemancade@outlook.com>
2022-01-15 15:36:35 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,5 @@
-local mainVape = 
+local function getScript(URL)
+    return game:HttpGet('https://raw.githubusercontent.com/Cadez650/VapeCustom/main/'..url, true)
+end
+
+local mainVape = getScript('main.vape')
